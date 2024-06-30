48e52d546dc8c2bb1103e583edba8a5407649d68
s3r6anita <xz200359@gmail.com>
2024-07-01 01:42:12 +0300

    create app with DI dependencies

---
+++ app/src/main/java/com/f4/starwarsfactsapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.f4.starwarsfactsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.f4.starwarsfactsapp.ui.theme.StarWarsFactsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StarWarsFactsApp()
+        }
+    }
+}
---
+++ app/src/main/java/com/f4/starwarsfactsapp/StarWarsFactsApp.kt
@@ -0,0 +1,11 @@
+package com.f4.starwarsfactsapp
+
+import androidx.compose.runtime.Composable
+import com.f4.starwarsfactsapp.ui.theme.StarWarsFactsAppTheme
+
+@Composable
+fun StarWarsFactsApp() {
+    StarWarsFactsAppTheme {
+
+    }
+}
